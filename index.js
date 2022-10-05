57db14ab35ffb3b0d4ccfdefd0e085d2b00c6cc9
muhanad53 <111211318+muhanad53@users.noreply.github.com>
2022-10-05 18:53:40 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-//السورس الاساسي لـ اظهار رابط موقعك في ريبل ات 
 const express = require("express");
 const app = express();
 app.listen(() => console.log("Server started"));
@@ -6,4 +5,3 @@ app.listen(() => console.log("Server started"));
 app.use('/ping', (req, res) => {
   res.send(new Date());
 });
-//https://ra3dstudio.com CopyRight Codes
